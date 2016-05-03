db12856e3024a12868847558528a1ce59e02a755
James Goatcher <jamesgoatcher@jamess-imac.attlocal.net>
2016-05-02 20:12:48 -0700

    wrote if else statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//conditions for matches
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again.")
+};
