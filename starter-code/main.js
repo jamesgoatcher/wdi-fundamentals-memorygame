d52846cb17b70816c4bfcd6418895f46d5aa7ed1
James Goatcher <jamesgoatcher@jamess-imac.attlocal.net>
2016-05-06 12:07:39 -0700

    created dynamic HTML with JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,10 +7,23 @@ var cardThree = "king";
 var cardFour = "king";
 
 //conditions for matches
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else if (cardThree === cardFour) {
 	alert("You found a match!")
 } else {
 	alert("Sorry, try again.")
+};*/
+
+//for loop to create the cards
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function () {
+for(var i = 0; i < 4; i++) {
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	document.getElementsByClassName('board')[0].appendChild(newCard);
+	}
 };
+
+createBoard();
